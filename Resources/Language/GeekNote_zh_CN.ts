6b6d42570723a046199cdf3442f7bd813a3e56c7
oasis-pioneer <166129150+OasisPioneer@users.noreply.github.com>
2025-10-19 14:54:56 +0800

    Add workspace panel components

--- Resources/Language/GeekNote_zh_CN.ts
+++ Resources/Language/GeekNote_zh_CN.ts
@@ -2,9 +2,51 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>LeftPanel</name>
+    <message>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="37"/>
+        <source>Project</source>
+        <translation type="unfinished">项目</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="48"/>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="63"/>
+        <source>Setting</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="65"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+</context>
+<context>
+    <name>RightPanel</name>
+    <message>
+        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="26"/>
+        <source>SearchProject</source>
+        <translation type="unfinished">搜索项目</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="27"/>
+        <source>New</source>
+        <translation type="unfinished">新建</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="28"/>
+        <source>Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="84"/>
+        <source>ProjectSettings</source>
+        <translation type="unfinished">项目设置</translation>
+    </message>
+</context>
+<context>
     <name>WorkSpace</name>
     <message>
-        <location filename="../../Src/WorkSpace.CPP" line="11"/>
+        <location filename="../../Src/WorkSpace/WorkSpace.CPP" line="14"/>
         <source>Work Space</source>
         <translation type="unfinished">工作空间</translation>
     </message>
