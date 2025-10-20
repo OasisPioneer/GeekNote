92a0fb3d7c026534b689b2d06a06f402a18441db
oasis-pioneer <166129150+OasisPioneer@users.noreply.github.com>
2025-10-20 13:31:20 +0800

    Optimize the interface

--- Resources/Language/GeekNote_zh_CN.ts
+++ Resources/Language/GeekNote_zh_CN.ts
@@ -4,41 +4,49 @@
 <context>
     <name>LeftPanel</name>
     <message>
-        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="37"/>
         <source>Project</source>
-        <translation type="unfinished">项目</translation>
+        <translation type="obsolete">项目</translation>
     </message>
     <message>
-        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="48"/>
-        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="63"/>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="45"/>
+        <source>NoteRepo</source>
+        <translation type="unfinished">笔记仓库</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="78"/>
+        <source>Options Menu</source>
+        <translation type="unfinished">选项菜单</translation>
+    </message>
+    <message>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="104"/>
         <source>Setting</source>
         <translation type="unfinished">设置</translation>
     </message>
     <message>
-        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="65"/>
+        <location filename="../../Src/WorkSpace/LeftPanel.CPP" line="106"/>
         <source>Exit</source>
         <translation type="unfinished">退出</translation>
     </message>
 </context>
 <context>
-    <name>RightPanel</name>
+    <name>NoteRepo</name>
     <message>
-        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="26"/>
+        <location filename="../../Src/WorkSpace/NoteRepo.CPP" line="31"/>
         <source>SearchProject</source>
         <translation type="unfinished">搜索项目</translation>
     </message>
     <message>
-        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="27"/>
+        <location filename="../../Src/WorkSpace/NoteRepo.CPP" line="32"/>
         <source>New</source>
         <translation type="unfinished">新建</translation>
     </message>
     <message>
-        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="28"/>
+        <location filename="../../Src/WorkSpace/NoteRepo.CPP" line="33"/>
         <source>Open</source>
         <translation type="unfinished">打开</translation>
     </message>
     <message>
-        <location filename="../../Src/WorkSpace/RightPanel.CPP" line="84"/>
+        <location filename="../../Src/WorkSpace/NoteRepo.CPP" line="92"/>
         <source>ProjectSettings</source>
         <translation type="unfinished">项目设置</translation>
     </message>
@@ -46,7 +54,7 @@
 <context>
     <name>WorkSpace</name>
     <message>
-        <location filename="../../Src/WorkSpace/WorkSpace.CPP" line="14"/>
+        <location filename="../../Src/WorkSpace/WorkSpace.CPP" line="15"/>
         <source>Work Space</source>
         <translation type="unfinished">工作空间</translation>
     </message>
